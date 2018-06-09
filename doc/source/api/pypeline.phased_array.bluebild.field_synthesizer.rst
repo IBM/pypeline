7a788d69e4be09fb76e287281426550dec4dad7d
Sepand KASHANI <kashani.sepand@gmail.com>
2018-06-09 08:26:35 +0200

    Add FieldSynthesizer abstract interface.

--- doc/source/api/pypeline.phased_array.bluebild.field_synthesizer.rst
+++ doc/source/api/pypeline.phased_array.bluebild.field_synthesizer.rst
@@ -2,15 +2,30 @@ pypeline.phased\_array.bluebild.field\_synthesizer
 ==================================================
 
 .. automodule:: pypeline.phased_array.bluebild.field_synthesizer
-
+   :special-members: __init__, __call__
    
    
    
 
    
    
+   .. rubric:: Classes
+
+   .. autosummary::
+   
+      FieldSynthesizerBlock
+   
    
 
    
-   
-   
+   .. rubric:: Modules
+
+   .. autosummary::
+      :toctree:
+
+      pypeline.phased_array.bluebild.field_synthesizer.spatial_domain
+
+
+
+
+
--- pypeline/phased_array/bluebild/field_synthesizer/__init__.py
+++ pypeline/phased_array/bluebild/field_synthesizer/__init__.py
@@ -7,3 +7,53 @@
 """
 Field synthesizers.
 """
+
+import pypeline.core as core
+
+
+class FieldSynthesizerBlock(core.Block):
+    """
+    Top-level public interface for Bluebild field synthesizers.
+    """
+
+    def __init__(self):
+        """
+
+        """
+        super().__init__()
+
+    def __call__(self, *args, **kwargs):
+        """
+        Compute instantaneous field statistics.
+
+        Parameters
+        ----------
+        *args
+            Positional arguments.
+        **kwargs
+            Keyword arguments.
+
+        Returns
+        -------
+        stat : :py:class:`~numpy.ndarray`
+            Field statistics.
+        """
+        raise NotImplementedError
+
+    def synthesize(self, *args, **kwargs):
+        """
+        Compute field values from statistics.
+
+        Parameters
+        ----------
+        *args
+            Positional arguments.
+        **kwargs
+            Keyword arguments.
+
+        Returns
+        -------
+        field : :py:class:`~numpy.ndarray`
+            Field values.
+        """
+        raise NotImplementedError
