44da84e98bd56cdde325cc38f074fed1ea3f3480
Sepand KASHANI <kashani.sepand@gmail.com>
2018-06-09 10:32:04 +0200

    Add multi-field synthesizer abstract interface.

--- doc/source/api/pypeline.phased_array.bluebild.imager.rst
+++ doc/source/api/pypeline.phased_array.bluebild.imager.rst
@@ -2,15 +2,27 @@ pypeline.phased\_array.bluebild.imager
 ======================================
 
 .. automodule:: pypeline.phased_array.bluebild.imager
+   :special-members: __init__, __call__
+
+
+
+
+
+
+   .. rubric:: Classes
+
+   .. autosummary::
+
+      IntegratingMultiFieldSynthesizerBlock
+
+
+
 
    
-   
-   
+   .. rubric:: Modules
 
-   
-   
-   
+   .. autosummary::
+      :toctree:
 
-   
-   
-   
+      pypeline.phased_array.bluebild.imager.spatial_domain
+
--- pypeline/phased_array/bluebild/imager/__init__.py
+++ pypeline/phased_array/bluebild/imager/__init__.py
@@ -5,5 +5,58 @@
 # #############################################################################
 
 """
-High-level Bluebild interface.
+High-level Bluebild interfaces.
 """
+
+import pypeline.core as core
+
+
+class IntegratingMultiFieldSynthesizerBlock(core.Block):
+    """
+    Top-level public interface of Bluebild multi-field synthesizers.
+    """
+
+    def __init__(self):
+        """
+
+        """
+        super().__init__()
+        self._statistics = None
+
+    def _update(self, stat):
+        if self._statistics is None:
+            self._statistics = stat
+        else:
+            self._statistics += stat
+
+    def __call__(self, *args, **kwargs):
+        """
+        Compute integrated field statistics for least-squares and standardized estimates.
+
+        Parameters
+        ----------
+        *args
+            Positional arguments.
+        **kwargs
+            Keyword arguments.
+
+        Returns
+        -------
+        stat : :py:class:`~numpy.ndarray`
+            Integrated field statistics.
+        """
+        raise NotImplementedError
+
+    def as_image(self):
+        """
+        Transform integrated statistics to viewable image.
+
+        Returns
+        -------
+        std : :py:class:`~pypeline.phased_array.util.io.SphericalImage`
+            (N_level, N_height, N_width) standardized energy-levels.
+
+        lsq : :py:class:`~pypeline.phased_array.util.io.SphericalImage`
+            (N_level, N_height, N_width) least-squares energy-levels.
+        """
+        raise NotImplementedError
